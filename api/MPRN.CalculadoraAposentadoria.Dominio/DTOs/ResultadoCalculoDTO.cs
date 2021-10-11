e7870f950b71786c77e5aef830a2faed235b9fc8
@lucas.felipe <l17.lucas@gmail.com>
2021-10-11 13:55:14 -0300

    criando layout do pdf

--- api/MPRN.CalculadoraAposentadoria.Dominio/DTOs/ResultadoCalculoDTO.cs
+++ api/MPRN.CalculadoraAposentadoria.Dominio/DTOs/ResultadoCalculoDTO.cs
@@ -8,6 +8,7 @@ namespace DTOs
         public Pessoa Pessoa {get;set;}
         public ResultadoCalculoAbono ResultadoCalculoAbono {get;set;}
         public ResultadoVerificacaoTempoIntegral ResultadoVerificacaoTempoIntegral {get;set;}
+        public ResultadoApenasTempoServico ResultadoApenasTempoServico { get; set; }
         public Document Doc { get; set; }
     }
 }
